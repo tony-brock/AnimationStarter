b93421a9aa1aa2af13064e82455a04f8e36e1bee
ajhowell2000 <anazjai@gmail.com>
2020-04-24 17:19:17 -0400

    NewAnimation

--- scatterPlots.js
+++ scatterPlots.js
@@ -1,3 +1,4 @@
+//get the mean grades 
 var getMeanGrade = function(entries)
 {
     return d3.mean(entries,function(entry)
@@ -6,13 +7,14 @@ var getMeanGrade = function(entries)
         })
 }
 
-
+// draw the scatter plot (update)
 var drawScatter = function(students,target,
               xScale,yScale,xProp,yProp)
 {
-
+    // sets the banner title and makes it upper case
     setBanner(xProp.toUpperCase() +" vs "+ yProp.toUpperCase());
     
+    //creates the points 
     d3.select(target).select(".graph")
     .selectAll("circle")
     .data(students)
@@ -28,7 +30,7 @@ var drawScatter = function(students,target,
     })
     .attr("r",4);
 }
-
+// gets rid of the points 
 var clearScatter = function(target)
 {
     d3.select(target)
@@ -37,8 +39,8 @@ var clearScatter = function(target)
         .remove();
 }
 
-
-var createAxes = function(screen,margins,graph,
+// creates the axes based on the size of the screen and the scales of the graph.
+var createAxes= function(screen,margins,graph,
                            target,xScale,yScale)
 {
     var xAxis = d3.axisBottom(xScale);
@@ -56,7 +58,37 @@ var createAxes = function(screen,margins,graph,
         .call(yAxis)
 }
 
+var initAxes= function(sreen,margins, graph, target, xScale, yScale){
+    
+    var axes= d3.select(target)
+            .append("g")
+            .classed("class", "axis");
+    
+    axes.append("g")
+        .attr("transform","translate("+margins.left+","
+             +(margins.top+graph.height)+")")
+        .attr("id", "xAxis")
+    
+    
+    axes.append("g")
+        .attr("id","yAxis")
+        .attr("transform","translate("+margins.left+","+(margins.top)+")")
+} 
 
+var updateAxes= function(target,xScale, yScale){
+    
+    var xAxis= d3.axisBottom(xScale);
+    var yAxis= d3.axisLeft(yScale); 
+    
+    d3.select("#xAxis")
+        .transition()
+        .call(xAxis)
+    
+    d3.select("#yAxis")
+        .transition()
+        .call(yAxis)
+}
+//  sets the size of the graph 
 var initGraph = function(target,students)
 {
     //the size of the screen
@@ -65,8 +97,8 @@ var initGraph = function(target,students)
     //how much space will be on each side of the graph
     var margins = {top:15,bottom:40,left:70,right:15};
     
-    //generated how much space the graph will take up
-    var graph = 
+    //generated how much space the graph will take up (update)
+    var graph  = 
     {
         width:screen.width-margins.left-margins.right,
         height:screen.height-margins.top-margins.bottom,
@@ -87,7 +119,7 @@ var initGraph = function(target,students)
         
     //create scales for all of the dimensions
     
-    
+    //Extra 
     var xScale = d3.scaleLinear()
         .domain([0,100])
         .range([0,graph.width])
@@ -97,8 +129,9 @@ var initGraph = function(target,students)
         .range([graph.height,0])
   
     
-    
-    createAxes(screen,margins,graph,target,xScale,yScale);
+    //calls the other functions to create the points, axes, and the title. 
+    initAxes(screen,margins,graph,target,xScale,yScale);
+    updateAxes(target,xScale,yScale);
     
     initButtons(students,target,xScale,yScale);
     
@@ -107,38 +140,38 @@ var initGraph = function(target,students)
     
 
 }
-
+// creates the buttons for the different data for the different graphs.
 var initButtons = function(students,target,xScale,yScale)
 {
-    
+    // button to create final vs HW plot
     d3.select("#fvh")
     .on("click",function()
     {
-        clearScatter(target);
+        
         drawScatter(students,target,
               xScale,yScale,"final","homework");
     })
-    
+    //button to create Hw vs test
     d3.select("#hvq")
     .on("click",function()
     {
-        clearScatter(target);
+        
         drawScatter(students,target,
               xScale,yScale,"homework","test");
     })
-    
+    // button to create test vs final
     d3.select("#tvf")
     .on("click",function()
     {
-        clearScatter(target);
+        
         drawScatter(students,target,
               xScale,yScale,"test","final");
     })
-    
+    // button to create test vs quiz
     d3.select("#tvq")
     .on("click",function()
     {
-        clearScatter(target);
+      
         drawScatter(students,target,
               xScale,yScale,"test","quizes");
     })
@@ -147,6 +180,7 @@ var initButtons = function(students,target,xScale,yScale)
     
 }
 
+// set the banner and its message 
 var setBanner = function(msg)
 {
     d3.select("#banner")
@@ -155,7 +189,7 @@ var setBanner = function(msg)
 }
 
 
-
+// loads in data
 var penguinPromise = d3.json("/classData.json");
 
 penguinPromise.then(function(penguins)
